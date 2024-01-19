617b33e42d3411c61fae12fa9de40890b34d9a76
Thiago_Reis <reist36@gmail.com>
2024-01-18 23:16:56 -0300

    Create: Category entity, service, repository and resource

---
+++ src/main/java/com/educandoweb/course/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.Category;
+import com.educandoweb.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	@Autowired
+	private CategoryRepository categoryRepository;
+
+	public List<Category> findAll() {
+		return categoryRepository.findAll();
+	}
+
+	public Category findById(Long id) {
+		Optional<Category> opt = categoryRepository.findById(id);
+		return opt.get();
+	}
+
+}
